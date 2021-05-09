80cf48214e8cb156d92bc6e3f09bb9c00a410dc7
Seo-won <41465465+Seo-won@users.noreply.github.com>
2021-05-10 05:24:42 +0900

    Update HogwartsSchool.js

--- HogwartsSchool.js
+++ HogwartsSchool.js
@@ -1,5 +1,5 @@
 function password(){
         pass=prompt('암호를 입력해주세요\n(답) 나는 천하의 멍텅구리임을 엄숙하게 선언합니다',''); // (답) 부분은 테스트, 발표 이후에는 지울 것임 
         if (pass=="나는 천하의 멍텅구리임을 엄숙하게 선언합니다") {location.href="http://bonozoa.dothome.co.kr/SchoolInt/Map.html";}
-        else {alert();}
+        else {alert('마법의 장난 끝!');}
     }
